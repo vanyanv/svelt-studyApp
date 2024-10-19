237c6a8f64403b6cd235f08a27ed092952acf46b
Vardan <vardan.vanyan@gmail.com>
2024-10-19 16:35:52 -0700

    user todos are now stored in local storage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,11 @@
 		}
 	});
 
+	//updating local Storage
+	function updateStorage() {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}
+
 	type toDO = {
 		toDo: string;
 		completed: boolean;
@@ -18,22 +23,25 @@
 
 	function addTodos() {
 		todos.unshift({ toDo: toDO, completed: false });
+		updateStorage();
 		todos = todos;
 	}
 
 	function removeATodo(index: number) {
 		todos = todos.filter((_, i) => i !== index);
+		updateStorage();
 	}
 
 	$: console.log(todos);
 </script>
 
+<h1>To do List</h1>
 <input type="text" bind:value={toDO} />
 <button on:click={addTodos}>Add a Todo</button>
 
 {#each todos as todo, index}
 	<div class="todo">
-		<input type="checkbox" bind:checked={todo.completed} />
+		<input type="checkbox" bind:checked={todo.completed} on:click={updateStorage} />
 		<p>{todo.toDo}</p>
 		<button on:click={() => removeATodo(index)}>Delete</button>
 	</div>
@@ -43,14 +51,15 @@
 	.todo {
 		display: flex;
 		align-items: center;
+		overflow-wrap: break-word;
 		gap: 15px;
 		height: 50px;
-		width: 100px;
+		width: 500px;
 	}
 
 	p {
 		color: goldenrod;
 		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
+		font-size: 1.5em;
 	}
 </style>
