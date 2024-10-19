1030f2b34b0184418332bdf19b53e3f41387851e
Vardan <vardan.vanyan@gmail.com>
2024-10-19 15:11:17 -0700

    created a remove todo function

--- src/components/Nested.svelte
+++ src/components/Nested.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
-	export let name;
+	export let names;
+
+	let m = { x: 0, y: 0 };
+
+	function handleMove(event: { clientX: number; clientY: number }) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
 </script>
 
-<h1>
-	Im a another component
-	{name}
-</h1>
+{#each names as name}
+	<h1>
+		Im a another component
+		{name}
+	</h1>
+{/each}
+
+<div on:pointermove={handleMove}>
+	The points is at {m.x} and {m.y}
+</div>
+
+<style>
+	div {
+		width: 500px;
+		height: 500px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,46 @@
 <script lang="ts">
 	import Nested from '../components/Nested.svelte';
 
-	let name = 'Svelt-Kit';
+	let toDO = '';
 	let names: string[] = [];
 
-	function addNames() {
-		names.push('Vardan');
+	function addTodos() {
+		names.unshift(toDO);
 		names = names;
+	}
+
+	function removeATodo(index: number) {
+		names = names.filter((_, i) => i !== index);
 	}
 </script>
 
-<h1>Welcome to {name}</h1>
+<!-- <h1>Welcome to {name}</h1> -->
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<input type="text" />
-<button on:click={addNames}>Add a Name</button>
-<Nested name= {names[0]} />
+<input type="text" bind:value={toDO} />
+<button on:click={addTodos}>Add a Todo</button>
+
+<!-- <Nested {names} /> -->
+<!-- {#if names.length > 0}
+	<p>{names[0]}</p>
+{/if} -->
+
+{#each names as name, index}
+	<div class="todo">
+		<p>{name}</p>
+		<button on:click={() => removeATodo(index)}>Delete</button>
+	</div>
+{/each}
 
 <style>
+	.todo {
+		display: flex;
+		align-items: center;
+		gap: 15px;
+		height: 50px;
+		width: 100px;
+	}
+
 	p {
 		color: goldenrod;
 		font-family: 'Comic Sans MS', cursive;
