0c8f713c5734899210897fb452e80dfcfed2c300
Vardan <vardan.vanyan@gmail.com>
2024-10-19 13:26:48 -0700

    working on understanding props

---
+++ src/components/Nested.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let name;
+</script>
+
+<h1>
+	Im a another component
+	{name}
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import Nested from '../components/Nested.svelte';
+
+	let name = 'Svelt-Kit';
+	let names: string[] = [];
+
+	function addNames() {
+		names.push('Vardan');
+		names = names;
+	}
+</script>
+
+<h1>Welcome to {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Working on Svelt</h2>
+
+<input type="text" />
+<button on:click={addNames}>Add a Name</button>
+<Nested name= {names[0]} />
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
